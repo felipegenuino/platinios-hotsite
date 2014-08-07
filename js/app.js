69253ab230857ab91c5f606ff78bb2f94ee8a04f
design@felipegenuino.com <91142298>
2014-08-07 08:14:19 -0300

    update components

--- js/app.js
+++ js/app.js
@@ -9,15 +9,6 @@ return false;
 });
 
 
-/* PRELOADER ==== */
-$(window).load(function() {
-'use strict';
-
-    /* Preloader ==== */
-      $('.spinner').fadeOut('slow');
-      $('.preloader').delay(350).fadeOut('slow');
-
-});
 
 
 
---
+++ js/components/preloader.js
@@ -0,0 +1,9 @@
+/* PRELOADER ==== */
+$(window).load(function() {
+'use strict';
+
+    /* Preloader ==== */
+      $('.spinner').fadeOut('slow');
+      $('.preloader').delay(350).fadeOut('slow');
+
+});
